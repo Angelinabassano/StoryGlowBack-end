7f5cf44184a3734d9c495a3b2adb8ab501b04968
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-09-12 00:17:33 +0200

    Fixes in serializer jpeg

--- book/serializer.py
+++ book/serializer.py
@@ -8,6 +8,6 @@ class BookSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'cover_photo']
 
     def validate_cover_photo(self, value):
-        if value and not value.name.lower().endswith(('.png', '.jpg')):
-            raise serializers.ValidationError("The file must be an image in PNG or JPG format")
+        if value and not value.name.lower().endswith(('.png', '.jpg', '.jpeg')):
+            raise serializers.ValidationError("The file must be an image in PNG, JPG, or JPEG format")
         return value
